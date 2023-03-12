6b8675d96b58d67c26284f6ca39001377eafda22
Lele253 <120642684+Lele253@users.noreply.github.com>
2023-03-12 20:35:11 +0100

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <HeaderComponent></HeaderComponent>
+    <v-row style="height: 600px;width: 100%; background-color: white">
+    </v-row>
+<v-row style="height: 400px; width: 100%" >
+  <v-col cols="4" style="background-color: black"></v-col>
+  <v-col cols="4" style="background-color: aquamarine"></v-col>
+  <v-col cols="4" style="background-color: aqua"></v-col>
+</v-row>
+
   </div>
 </template>
 

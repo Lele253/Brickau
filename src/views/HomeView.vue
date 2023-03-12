44002fccc8f50f1e0dd41a44e6c82387c95f21a4
Lele253 <120642684+Lele253@users.noreply.github.com>
2023-03-12 20:53:02 +0100

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
     <HeaderComponent></HeaderComponent>
-    <v-row style="height: 600px;width: 100vw; background-color: white">
+    <v-row style="height: 800px;width: 100vw; background-color: white"></v-row>
+
+    <v-row style="height: 600px; width: 100vw">
+      <v-col cols="4" style="background-color: black" xs="12"></v-col>
+      <v-col cols="4" style="background-color: aquamarine" xs="12"></v-col>
+      <v-col cols="4" style="background-color: aqua" xs="12"></v-col>
     </v-row>
-    <v-row style="height: 400px; width: 100vw">
-      <v-col cols="4" style="background-color: black"></v-col>
-      <v-col cols="4" style="background-color: aquamarine"></v-col>
-      <v-col cols="4" style="background-color: aqua"></v-col>
+
+    <v-row style="height: 800px;width: 100vw; background-color: white"></v-row>
+
+    <v-row style="height: 600px">
+      <v-col cols="6">
+        <v-img src="../assets/logo.png" style="max-height: 600px"/>
+      </v-col>
+      <v-col cols="6" style="background-color: aqua"></v-col>
     </v-row>
 
   </div>

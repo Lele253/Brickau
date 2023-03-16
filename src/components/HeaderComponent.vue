efa1043d4b15127a88b82b834cea499192610ab0
Tim Thorben Föhrs <tim.thorben17@gmail.com>
2023-03-16 15:41:45 +0100

    Removed HeaderComponent.vue BurgerMenu

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,84 +7,12 @@
         </v-app-bar-title>
       </v-col>
       <v-col></v-col>
-      <v-col class="d-flex justify-end align-center" style="height: 70px">
-        <v-app-bar-nav-icon class="mr-sm-5 mt-2 text-sm-h6">
-          <Icon class="icon" icon="mdi:hamburger-menu" style="cursor: pointer" @click="drawer=!drawer"/>
-        </v-app-bar-nav-icon>
-      </v-col>
-      <!--      <v-col class="d-flex justify-end align-center">
-
-              <Icon v-if="!user" class="icon mt-3 mr-3" icon="oi:account-login" style="cursor: pointer"
-                    @click="$router.push('/login')"/>
-              <v-dialog
-                  v-model="dialog"
-                  max-width="290"
-                  persistent
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <Icon v-if="user" class="icon mt-3 mr-3" icon="oi:account-logout" style="cursor: pointer"
-                        v-bind="attrs" @click="dialog=true" v-on="on"/>
-                </template>
-                <v-card>
-                  <v-card-text class="text-h5">
-                    <span>Möchtest du dich<br> wirklich Abmelden?</span>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        color="green darken-1"
-                        text
-                        @click="dialog = false"
-                    >
-                      Nein
-                    </v-btn>
-                    <v-btn
-                        color="green darken-1"
-                        text
-                        @click="logout"
-                    >
-                      Ja
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-col>-->
     </v-row>
   </v-app-bar>
-
-  <v-navigation-drawer v-model="drawer" fixed location="right"
-                       style="background-color: grey; width: 20%; height: 100vh" temporary>
-
-
-    <v-list>
-      <v-divider></v-divider>
-      <v-list-item v-for="i in list" :key="i" class="d-flex align-center" link style="height: 80px"
-                   @click="$router.push(i.path)">
-        <v-row>
-          <v-col cols="1">
-            <Icon :icon="i.icon" style="font-size: 30px"/>
-          </v-col>
-          <v-col cols="9">
-            <p class="ml-2" style="font-size: 20px">
-              {{ i.name }}
-            </p></v-col>
-        </v-row>
-      </v-list-item>
-      <v-list-item class="d-flex justify-center">
-        <v-btn style="margin-top: 600px; width: 200px" @click="drawer=!drawer">
-          Logout
-        </v-btn>
-      </v-list-item>
-      <v-divider></v-divider>
-    </v-list>
-
-
-  </v-navigation-drawer>
 
 </template>
 
 <script>
-import {Icon} from '@iconify/vue';
 import {mapGetters} from "vuex";
 import axios from "axios";
 
@@ -93,9 +21,7 @@ export default {
     ...mapGetters(['user'])
   },
   name: "HeaderComponent",
-  components: {
-    Icon,
-  },
+  components: {},
   data() {
     return {
       list: [

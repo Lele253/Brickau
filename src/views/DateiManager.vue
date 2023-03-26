33fc4a9eae222fe53eca3b7aabd8697cbef6d784
Stefan <stefan.franke@itc-studenten.de>
2023-03-26 10:37:22 +0200

    login etc

--- src/views/DateiManager.vue
+++ src/views/DateiManager.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-      <h1 v-for="x in dateien" :key="x" class="text-white">{{x.name}}  befindet sich im ordner:  {{x.ordner}}</h1>
+      <h1 @click="löschpfad = x.path " v-for="x in dateien" :key="x" class="text-white">{{x.name}}  befindet sich im ordner:  {{x.ordner}}</h1>
+    {{löschpfad}}
   </div>
+  <v-btn @click="delteDatei">delete</v-btn>
 </template>
 
 <script>
@@ -15,10 +17,22 @@ export default {
   data(){
     return{
     dateien:[],
-
+      löschpfad:'teasdajhd'
     }
   },
   methods:{
+    delteDatei(){
+       axios.post('http://localhost:8080/auth/deleteFile', { message: this.löschpfad })
+            .then(response => {
+              console.log(response.data);
+              this.getAllData()
+              return response.data;
+            })
+            .catch(error => {
+              console.error(error);
+            });
+    },
+
     async getAllData() {
         const response = await axios.get("http://localhost:8080/auth/alleDateien", {
 

01b18da6e6e764bb906e42de2d2d2b2b8757dac6
Stefan <stefan.franke@itc-studenten.de>
2023-03-29 12:53:07 +0200

    Datei Upload warte anzeige

---
+++ src/views/TestView.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <video id="webcam"></video>
+</div>
+</template>
+
+<script>
+ navigator.mediaDevices.getUserMedia({video:true}).then(stream => {
+   let webcam = document.querySelector("#webcam");
+   webcam.srcObject = stream; webcam.play()})
+export default {
+  name: "TestView"
+}
+</script>
+
+<style scoped>
+#webcam{
+  width: 400px;
+  height: 400px;
+}
+</style>
